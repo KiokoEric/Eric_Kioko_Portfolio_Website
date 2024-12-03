92ab67eebc3f92b251328039dbfefa6539773129
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-12-03 11:21:37 +0300

    Update of the Tailwind.config.js File

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Grey: 'rgba(128, 128, 128, 0.35)',
+      },
+      width: {
+        custom: '260px',
+        tablet: '700px',
+        contact: '850px'
+      },
+      height: {
+        custom: '260px'
+      }
+    },
   },
   plugins: [],
 }
-
