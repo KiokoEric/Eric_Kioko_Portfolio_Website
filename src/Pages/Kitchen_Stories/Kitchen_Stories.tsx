252eab7b0ba6d80ba9a27bfaf720fa95be48dc4b
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-12-09 14:18:33 +0300

    Update of the Kitchen_Stories Page

--- src/Pages/Kitchen_Stories/Kitchen_Stories.tsx
+++ src/Pages/Kitchen_Stories/Kitchen_Stories.tsx
@@ -1,7 +1,7 @@
-import 'swiper/css';
+import 'swiper.css';
 import React from 'react';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-coverflow';
+import 'swiper.pagination.css';
+import 'swiper/effect-coverflow.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 import Home from "../../assets/Kitchen_Stories/HomePage.jpeg";
