97de164ec20fc9b7078ddab0edd70d351db3e970
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-12-09 14:08:31 +0300

    Update of the Better_Health Page

--- src/Pages/Better_Health/Better_Health.tsx
+++ src/Pages/Better_Health/Better_Health.tsx
@@ -1,7 +1,7 @@
-import 'swiper/css';
+import 'swiper.css';
 import React from 'react';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-coverflow';
+import 'swiper.pagination.css';
+import 'swiper/effect-coverflow.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 import Home_Page from "../../assets/Better_Health/Home_Page.jpeg";
